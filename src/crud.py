8de5138ee9719e29c8e7884598239015f81a5c37
Fellype <fellypegabryelalmeidalima@gmail.com>
2025-09-24 19:23:10 -0300

    crud att

--- src/crud.py
+++ src/crud.py
@@ -1,49 +1,39 @@
 import sqlite3
-from aluno import Aluno
+from banco import get_connection  # Conexão com o banco
+from aluno import Aluno           # Modelo Aluno
 
 DB_PATH = 'src/alunos.db'
 
 class Crud:
-    def __init__(self):
-        self.conexao = sqlite3.connect(DB_PATH)
-        self.cursor = self.conexao.cursor()
 
-    def __del__(self):
-        self.conexao.close()
+    def create_aluno(aluno):
+        sql = """INSERT INTO aluno (nome, cpf, data_nascimento, status)
+        VALUES (?,?,?,?)"""
+        with get_connection() as conn:
+            cur = conn.execute(sql, (aluno.nome, aluno.cpf,
+            aluno.data_nascimento, aluno.status))
+            return cur.lastrowid
+    
+    def read_aluno_by_id(id):
+        sql = "SELECT * FROM aluno WHERE id = "
+        with get_connection() as conn:
+            cur = conn.execute(sql, (id,))
+            return cur.fetchone() # Retorna None se não encontrar
 
-    def criar_aluno(self, aluno: Aluno):
-        self.cursor.execute("INSERT INTO Aluno (nome, cpf, data_nascimento, telefone) VALUES (?, ?, ?, ?)",
-            (aluno.nome, aluno.cpf, aluno.data_nascimento, aluno.telefone))
-        self.conexao.commit()
-        return self.cursor.lastrowid
-
-    def ler_alunos(self):
-        self.cursor.execute("SELECT id, nome, cpf, data_nascimento, telefone FROM Aluno")
-        alunos_data = self.cursor.fetchall()
-        alunos = []
-        for data in alunos_data:
-            aluno = Aluno(data[1], data[2], data[3], data[4])
-            aluno.id = data[0] # Adiciona o ID ao objeto Aluno
-            alunos.append(aluno)
-        return alunos
-
-    def ler_aluno_por_id(self, aluno_id):
-        self.cursor.execute("SELECT id, nome, cpf, data_nascimento, telefone FROM Aluno WHERE id = ?", (aluno_id,))
-        data = self.cursor.fetchone()
-        if data:
-            aluno = Aluno(data[1], data[2], data[3], data[4])
-            aluno.id = data[0]
-            return aluno
-        return None
-
-    def atualizar_aluno(self, aluno_id, aluno: Aluno):
-        self.cursor.execute("UPDATE Aluno SET nome = ?, cpf = ?, data_nascimento = ?, telefone = ? WHERE id = ?",
-            (aluno.nome, aluno.cpf, aluno.data_nascimento, aluno.telefone, aluno_id))
-        
-        self.conexao.commit()
-        return self.cursor.rowcount > 0
-
-    def deletar_aluno(self, aluno_id):
-        self.cursor.execute("DELETE FROM Aluno WHERE id = ?", (aluno_id,))
-        self.conexao.commit()
-        return self.cursor.rowcount > 0
+    def read_alunos():
+        sql = "SELECT * FROM aluno ORDER BY id DESC"
+        with get_connection() as conn:
+            cur = conn.execute(sql)
+            return cur.fetchall()
+    
+    def update_aluno(id, novos_dados):
+        sql = "UPDATE aluno SET status = ? WHERE id = ?"
+        with get_connection() as conn:
+            cur = conn.execute(sql, (novos_dados["status"], id))
+            return cur.rowcount > 0 # True se atualizou, False se não encontrou
+    
+    def delete_aluno(id):
+        sql = "DELETE FROM aluno WHERE id = ?"
+        with get_connection() as conn:
+            cur = conn.execute(sql, (id,))
+            return cur.rowcount > 0
